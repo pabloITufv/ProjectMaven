2fe017982e4aaf910923ccbf52a34a677e3332b8
“PabloC” <7736022@alumnos.ufv.es>
2022-10-20 20:33:46 +0200

    Runeando

--- src/main/java/com/pruebaCSV/PruebaCSV.java
+++ src/main/java/com/pruebaCSV/PruebaCSV.java
@@ -10,7 +10,7 @@ public class PruebaCSV {
      *
      */
     public static void main(String[] args) {
-        //LectorCSV miLector = new LectorCSV();
-        //miLector.leeCSV("./datos/pokemon.csv");
+        LectorCSV miLector = new LectorCSV();
+        miLector.leeCSV("./datos/pokemon.csv");
     }
 }
